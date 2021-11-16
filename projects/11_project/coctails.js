6b2bc17f7efdfc4d5a541e4755429cd3d8e2ec71
Ilya <kexit353@gmail.com>
2021-11-16 18:10:29 +0300

    16.11

---
+++ projects/11_project/coctails.js
@@ -0,0 +1,13 @@
+class HashStorage {
+    constructor() {}
+
+    addValue(key, value) {}
+
+    getValue(key) {}
+
+    deleteValue(key) {}
+
+    getKeys() {}
+}
+
+class coctailsStorage extends HashStorage {}
